4d7e0bc78b9d2d8cd580992a37dbe8d282397133
Rune Lund-Søltoft <lundsoltoft@gmail.com>
2019-10-01 14:06:14 +0200

    key creation and verification works

--- hobbes.server/src/Server.fs
+++ hobbes.server/src/Server.fs
@@ -60,9 +60,12 @@ let encrypt (plainText : string) =
     rijAlg.IV <- initializationVector
     let encryptor = rijAlg.CreateEncryptor(rijAlg.Key, rijAlg.IV)
     use msEncrypt = new MemoryStream()
-    use csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write)
-    use swEncrypt = new StreamWriter(csEncrypt)
-    swEncrypt.Write(plainText)
+    (
+        use csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write)
+        use swEncrypt = new StreamWriter(csEncrypt)
+        swEncrypt.Write(plainText)
+    )
+    msEncrypt.Close()
     msEncrypt.ToArray()
     |> toB64
 
@@ -73,9 +76,11 @@ let decrypt (base64Text : string) =
     rijAlg.IV <- initializationVector
     let decryptor = rijAlg.CreateDecryptor(rijAlg.Key, rijAlg.IV)
     use msDecrypt = new MemoryStream(cipherText)
-    use csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read)
-    use srDecrypt = new StreamReader(csDecrypt)
-    srDecrypt.ReadToEnd()
+    (
+        use csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read)
+        use srDecrypt = new StreamReader(csDecrypt)
+        srDecrypt.ReadToEnd()
+    )
 
 let private createToken (user : UserRecord.Root) = 
     let header = 

06c626a5bc2fc46062c89ef76a495d03cc56586c
Rune Lund-Søltoft <lundsoltoft@gmail.com>
2019-11-01 12:40:00 +0100

    Forgot to reflect previous changes in the workbench removed log arguments that we aren't using at present

--- hobbes.server/src/db/Log.fs
+++ hobbes.server/src/db/Log.fs
@@ -1,57 +1,58 @@
 namespace Hobbes.Server.Db
 
 module Log =
-   let mutable logger = fun (_:string) -> ()
-   open FSharp.Core.Printf
+    //need to make it possible to bootstrap the logger, since the db is also using the logger
+    let mutable logger = eprintfn "%s"
 
-   type LogType = 
-       Info
-       | Debug
-       | Error
-       | Unknown
-       with override x.ToString() = 
-               match x with
-               Info      -> "info"
-               | Debug   -> "debug"
-               | Error   -> "error"
-               | Unknown -> "unknown"
-            static member Parse (s:string) =
-                   match s.ToLower() with
-                   "info"      -> Info
-                   | "debug"   -> Debug
-                   | "error"   -> Error
-                   | "unknown" -> Unknown
-                   | _         -> 
-                       eprintfn "Unknown sync state: %s" s
-                       Unknown
-                       
-   let private writeLogMessage timeStampID (logType : LogType) requestID stacktrace msg =
-       let doc = sprintf """{"timestamp" : "%s",
-                            "type" : "%A",
-                            "requestId" : "%i",,
-                            "stacktrace" : "%s"
-                            "message" : "%s"}""" timeStampID logType requestID stacktrace msg
-       async {
-          try
-             doc |> logger
-          with e ->
-              eprintfn "Failedto insert log doc %s. Message: %s StackTRace %s" doc e.Message e.StackTrace
-       } |> Async.Start
+    open FSharp.Core.Printf
 
-   let log msg =
-       writeLogMessage null Info -1 null msg
+    type LogType = 
+        Info
+        | Debug
+        | Error
+        | Unknown
+        with override x.ToString() = 
+                match x with
+                Info      -> "info"
+                | Debug   -> "debug"
+                | Error   -> "error"
+                | Unknown -> "unknown"
+             static member Parse (s:string) =
+                match s.ToLower() with
+                "info"      -> Info
+                | "debug"   -> Debug
+                | "error"   -> Error
+                | "unknown" -> Unknown
+                | _         -> 
+                    eprintfn "Unknown sync state: %s" s
+                    Unknown
 
-   let error stacktrace msg = 
-       writeLogMessage  null Error -1 stacktrace msg
-
-   let debug msg  =
-       writeLogMessage null Debug -1 null msg
-
-   let logf  format =
-      ksprintf ( writeLogMessage null Info -1 null) format
-      
-   let errorf stacktrace format = 
-      ksprintf (writeLogMessage null Error -1 stacktrace) format
-
-   let debugf format =
-       ksprintf ( writeLogMessage null Debug -1 null) format
+    let private writeLogMessage (logType : LogType) stacktrace msg =
+        let doc = sprintf """{"timestamp" : "%s",
+                             "type" : "%A",
+                             "stacktrace" : "%s",
+                             "message" : "%s"}""" (System.DateTime.Now.ToString(System.Globalization.CultureInfo.InvariantCulture)) logType stacktrace msg
+        async {
+           try
+              doc |> logger
+           with e ->
+               eprintfn "Failedto insert log doc %s. Message: %s StackTRace %s" doc e.Message e.StackTrace
+        } |> Async.Start
+ 
+    let log msg =
+        writeLogMessage Info null msg
+ 
+    let error stacktrace msg = 
+        writeLogMessage  Error stacktrace msg
+ 
+    let debug msg  =
+        writeLogMessage Debug null msg
+ 
+    let logf  format =
+       ksprintf ( writeLogMessage Info null) format
+       
+    let errorf stacktrace format = 
+       ksprintf (writeLogMessage Error stacktrace) format
+ 
+    let debugf format =
+        ksprintf ( writeLogMessage Debug null) format

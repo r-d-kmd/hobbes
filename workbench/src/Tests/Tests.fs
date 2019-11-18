f80f902173ac068aa67438a773931546fe427d4b
Rune Lund-Søltoft <lundsoltoft@gmail.com>
2019-11-18 14:26:38 +0100

    forgot to correct workbench before pushing

--- workbench/src/Tests/Tests.fs
+++ workbench/src/Tests/Tests.fs
@@ -1,16 +1,20 @@
 module Workbench.Tests
 open Hobbes.Server.Db
+open Hobbes.Server.Services
+
+let env name = 
+    System.Environment.GetEnvironmentVariable name
 
 let getData conf = 
-    let statusCode,res = Implementation.csv conf
-    res.Substring(0, min 500 res.Length) |> printf "Status: %d. %A" statusCode
+    let _,res = Hobbes.Server.Services.Data.csv conf
+    res.Substring(0, min 500 res.Length) |> printf "Csv: %A" 
     
 let cacheInvalidation configName = 
     DataConfiguration.AzureDevOps configName //run these two lines of code to test cache invalidation
     |> Cache.invalidateCache
 
 let sync configuration = 
-    let _,key = Hobbes.Server.Readers.AzureDevOps.sync "y3cg7xrajppvd4b2wp6ahrgnsdkpf4sidtlinthcwepc2pjbzfuq" ("kmddk","gandalf") "abcd"
+    let _,key = Hobbes.Server.Readers.AzureDevOps.sync (env "AZURE_TOKEN") ("kmddk","gandalf") "abcd"
     printfn "sync key: %s" key
 
 let thingy () =

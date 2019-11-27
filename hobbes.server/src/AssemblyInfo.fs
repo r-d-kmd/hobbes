ce3e6a0abccfb8b1757965a416a186ce303711e9
Rune Lund-Søltoft <lundsoltoft@gmail.com>
2019-11-27 22:10:06 +0100

    trying to find the error in the production environment. Might be because hobbes starts faster than the db, and the db is thus not configured correctly

--- hobbes.server/src/AssemblyInfo.fs
+++ hobbes.server/src/AssemblyInfo.fs
@@ -4,7 +4,7 @@ open System.Reflection
 
 [<assembly: AssemblyVersionAttribute("1.1")>]
 [<assembly: AssemblyTitleAttribute("Hobbes server")>]
-[<assembly: AssemblyMetadataAttribute("hash","6b13b1")>]
+[<assembly: AssemblyMetadataAttribute("hash","d96667")>]
 [<assembly: AssemblyFileVersionAttribute("1.1")>]
 [<assembly: AssemblyProductAttribute("Hobbes server")>]
 do ()
@@ -12,6 +12,6 @@ do ()
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyVersion = "1.1"
     let [<Literal>] AssemblyTitle = "Hobbes server"
-    let [<Literal>] AssemblyMetadata_hash = "6b13b1"
+    let [<Literal>] AssemblyMetadata_hash = "d96667"
     let [<Literal>] AssemblyFileVersion = "1.1"
     let [<Literal>] AssemblyProduct = "Hobbes server"

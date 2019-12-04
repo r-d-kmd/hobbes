c29b94fef8aa0ba71123a56efb7d2ae774ca26c5
Mikkel Ditlevsen (DIT) <DIT@kmd.dk>
2019-12-04 19:56:40 +0100

    forgot --platform linux in local build script for collector

--- hobbes.server/src/AssemblyInfo.fs
+++ hobbes.server/src/AssemblyInfo.fs
@@ -4,7 +4,7 @@ open System.Reflection
 
 [<assembly: AssemblyVersionAttribute("1.1")>]
 [<assembly: AssemblyTitleAttribute("Hobbes server")>]
-[<assembly: AssemblyMetadataAttribute("hash","5ea7b7")>]
+[<assembly: AssemblyMetadataAttribute("hash","82ba47")>]
 [<assembly: AssemblyFileVersionAttribute("1.1")>]
 [<assembly: AssemblyProductAttribute("Hobbes server")>]
 do ()
@@ -12,6 +12,6 @@ do ()
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyVersion = "1.1"
     let [<Literal>] AssemblyTitle = "Hobbes server"
-    let [<Literal>] AssemblyMetadata_hash = "5ea7b7"
+    let [<Literal>] AssemblyMetadata_hash = "82ba47"
     let [<Literal>] AssemblyFileVersion = "1.1"
     let [<Literal>] AssemblyProduct = "Hobbes server"

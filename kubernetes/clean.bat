e457f7bcb9a97aecf754796904e7440c2d50a3e7
Mikkel Ditlevsen (DIT) <DIT@kmd.dk>
2019-12-04 12:20:36 +0100

    kuber

--- kubernetes/clean.bat
+++ kubernetes/clean.bat
@@ -1,5 +1,4 @@
-kubectl delete --all deployment --all-namespaces
-kubectl delete --all service --all-namespaces
-kubectl delete --all pods --all-namespaces
-kubectl delete --all ingress --all-namespaces
-kubectl delete --all namespaces --all-namespaces
+kubectl delete --all deployment
+kubectl delete --all service
+kubectl delete --all pods
+kubectl delete --all pvc

b260f8c8ac5e2a7f9ebfc7c55dfe29dc4096b02b
Mikkel Ditlevsen (DIT) <DIT@kmd.dk>
2019-12-04 15:58:43 +0100

    jens

--- hobbes.server/src/AssemblyInfo.fs
+++ hobbes.server/src/AssemblyInfo.fs
@@ -4,7 +4,7 @@ open System.Reflection
 
 [<assembly: AssemblyVersionAttribute("1.1")>]
 [<assembly: AssemblyTitleAttribute("Hobbes server")>]
-[<assembly: AssemblyMetadataAttribute("hash","79156f")>]
+[<assembly: AssemblyMetadataAttribute("hash","5ea7b7")>]
 [<assembly: AssemblyFileVersionAttribute("1.1")>]
 [<assembly: AssemblyProductAttribute("Hobbes server")>]
 do ()
@@ -12,6 +12,6 @@ do ()
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyVersion = "1.1"
     let [<Literal>] AssemblyTitle = "Hobbes server"
-    let [<Literal>] AssemblyMetadata_hash = "79156f"
+    let [<Literal>] AssemblyMetadata_hash = "5ea7b7"
     let [<Literal>] AssemblyFileVersion = "1.1"
     let [<Literal>] AssemblyProduct = "Hobbes server"

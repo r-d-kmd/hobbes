912ef8fb3c6ccc71d92f3709457c56b0b6840f5b
Mikkel Ditlevsen (DIT) <DIT@kmd.dk>
2019-12-09 16:37:55 +0100

    Lucas added kubernetes objects for azuredevops server and database. It works

--- collectors/collectors.azuredevops/src/Server.fs
+++ collectors/collectors.azuredevops/src/Server.fs
@@ -30,7 +30,7 @@ let private app = application {
 let rec private init() =
     async {
         try
-           FSharp.Data.Http.Request("http://collectordb:5984") |> ignore //make sure db is up and running
+           FSharp.Data.Http.Request("http://collectordb-svc:5984") |> ignore //make sure db is up and running
            initDb() |> ignore
            printfn "DB initialized"
         with _ ->

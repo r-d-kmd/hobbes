d0796962ce829602c9c42a3728eb58f30a5ea471
Rune Lund-Søltoft <funchsoltoft@gmail.com>
2019-12-13 12:25:59 +0100

    Commented expandingCompletionBySprint to help with understanding how to use hobbes Added a last index by statement to the above transformation to populate the sprint number column

--- workbench/src/production transformations/Metrics.fs
+++ workbench/src/production transformations/Metrics.fs
@@ -20,19 +20,20 @@ module Metrics =
     [<Workbench.Transformation 1>]
     let expandingCompletionBySprint =
         [
+            //remove all other columns than those metioned
             slice columns [SprintNumber.Name; "Done"]
+            //index the rows by sprint number
             index rows by SprintNumber.Expression
+            //sort by the sprint number column 
             sort by SprintNumber.Name
+            //Create a column called Burn up thats the expanding sum ie running total of the done column
             create (column "Burn up") (expanding Sum (!> "Done")) 
+            //Create a column named Velocity that's the moving mean of 'Done' of the last three rows
             create (column "Velocity") ((moving Mean 3 (!> "Done")))
+            //Create a column called Burn up Prediction thats a linear extrapolation ten rows ahead based on the last ten rows of the data set
             create (column "Burn up Prediction") ((linear extrapolationLimited) (!> "Burn up") 10 10)
-            slice columns [
-                            SprintNumber.Name
-                            "Done"
-                            "Velocity"
-                            "Burn up"
-                            "Burn up Prediction"
-                          ]
+            //required to populate the Sprint number column with the predicted values
+            index rows by SprintNumber.Expression
         ]
         
     [<Workbench.Transformation 1>]

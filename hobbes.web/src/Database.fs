79d66759bd37a70dd1cd5b3d75b13f0dba003a37
Mikkel Ditlevsen (DIT) <DIT@kmd.dk>
2020-01-24 11:27:40 +0100

    trying keepalive true again

--- hobbes.web/src/Database.fs
+++ hobbes.web/src/Database.fs
@@ -225,7 +225,7 @@ namespace Hobbes.Web
                 let headers =
                     [
                         yield HttpRequestHeaders.BasicAuth user pwd
-                        yield HttpRequestHeaders.KeepAlive "false"
+                        yield HttpRequestHeaders.KeepAlive "true"
                         yield HttpRequestHeaders.ContentType HttpContentTypes.Json
                         if rev |> Option.isSome then yield HttpRequestHeaders.IfMatch rev.Value
                     ]

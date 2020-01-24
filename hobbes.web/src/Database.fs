bb243ddd906a3ef303ef4287ecbd70950595099c
Mikkel Ditlevsen (DIT) <DIT@kmd.dk>
2020-01-24 10:42:15 +0100

    trying to fix bug where collector crashes after sucessfully syncing when updating the sync doc

--- hobbes.web/src/Database.fs
+++ hobbes.web/src/Database.fs
@@ -225,6 +225,7 @@ namespace Hobbes.Web
                 let headers =
                     [
                         yield HttpRequestHeaders.BasicAuth user pwd
+                        HttpRequestHeaders.KeepAlive "true"
                         yield HttpRequestHeaders.ContentType HttpContentTypes.Json
                         if rev |> Option.isSome then yield HttpRequestHeaders.IfMatch rev.Value
                     ]

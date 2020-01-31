a9ba91efe7abafd49d42ac533daea5abf93a9536
Mikkel Ditlevsen (DIT) <DIT@kmd.dk>
2020-01-31 10:36:26 +0100

    Trying to get Iteration.IterationPath to work

--- kubernetes/startkube.bat
+++ kubernetes/startkube.bat
@@ -1,3 +1,3 @@
-minikube start --vm-driver hyperv --hyperv-virtual-switch "Primary Virtual Switch"
+minikube start --vm-driver hyperv --hyperv-virtual-switch "Primary Virtual Switch" --memory=4096
 @echo off
 pause

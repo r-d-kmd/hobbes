879360ea5efce6f77829ec04a67dfbc847c76aa2
Mikkel Ditlevsen (DIT) <DIT@kmd.dk>
2020-04-16 11:37:22 +0200

    updated the Update functión in functions.sh, also deleted all the sh files

--- kubernetes/restart.sh
+++
@@ -1,2 +0,0 @@
-kubectl scale --replicas=0 -f $1-deployment.yaml
-kubectl scale --replicas=1 -f $1-deployment.yaml 
--- kubernetes/start.sh
+++
@@ -1,6 +0,0 @@
-eval $(minikube -p minikube docker-env)
-cd .. && fake build
-cd kubernetes
-kubectl apply -f env.JSON
-kubectl apply -f db-deployment.yaml,db-svc.yaml,db-volume.yaml,hobbes-deployment.yaml,hobbes-svc.yaml,collectordb-volume.yaml,collectordb-deployment.yaml,collectordb-svc.yaml,azuredevops-deployment.yaml,azuredevops-svc.yaml
-#open "http://$(minikube ip):30080"

626a9e427cb2135ad96036b9878653bd2389365a
Rune Lund-Søltoft <funchsoltoft@gmail.com>
2020-04-21 11:20:30 +0200

    rewritting the git collector fixing an error in the test of csv

--- collectors/collectors.Git/src/Reader.fs
+++ collectors/collectors.Git/src/Reader.fs
@@ -1,104 +1,246 @@
 namespace Collector.Git
-open LibGit2Sharp
 open Hobbes.Web.Log
+open FSharp.Data
+open Hobbes.Helpers.Environment
 
 module Reader =
 
-    let private hash (input : string) =
-            use md5Hash = System.Security.Cryptography.MD5.Create()
-            let data = md5Hash.ComputeHash(System.Text.Encoding.UTF8.GetBytes(input))
-            let sBuilder = System.Text.StringBuilder()
-            (data
-            |> Seq.fold(fun (sBuilder : System.Text.StringBuilder) d ->
-                    sBuilder.Append(d.ToString("x2"))
-            ) sBuilder).ToString()
+    let user = env "GIT_AZURE_USER" null
+    let pwd = env "GIT_AZURE_PASSWORD" null
+
+    type private CommitBatch = JsonProvider<"""{
+        "count": 100,
+        "value": [
+            {
+                "commitId": "b1c561d96ff1808700b34b3ef5346e0df4bf5ed8",
+                "author": {
+                    "name": "Jakob Pele Leer",
+                    "email": "JL@kmd.dk",
+                    "date": "2020-04-21T07:17:10Z"
+                },
+                "committer": {
+                    "name": "Jakob Pele Leer",
+                    "email": "JL@kmd.dk",
+                    "date": "2020-04-21T07:17:10Z"
+                },
+                "comment": "Merged PR 37570: Student member submitting form about end of education only registers",
+                "commentTruncated": true,
+                "changeCounts": {
+                    "Add": 0,
+                    "Edit": 5,
+                    "Delete": 0
+                },
+                "url": "https://dev.azure.com/kmddk/2139bb34-57e3-4d7d-a6e1-1c0542a45e29/_apis/git/repositories/897458ba-1c7f-40c8-8dc0-043f083c6cd7/commits/b1c561d96ff1808700b34b3ef5346e0df4bf5ed8",
+                "remoteUrl": "https://dev.azure.com/kmddk/Gandalf/_git/MemberUI/commit/b1c561d96ff1808700b34b3ef5346e0df4bf5ed8"
+            }]
+    }""">
+    type private BranchList = JsonProvider<"""{
+        "value": [{
+            "name": "refs/heads/47902_fix_f_ui_nightly_tests_pt3",
+            "objectId": "0ba81f799d711fd6dfda9f39d3224cc30869bc57",
+            "creator": {
+                "displayName": "Karolina Michon (KLM)",
+                "url": "https://spsprodweu1.vssps.visualstudio.com/A5171eb22-e5ac-4dc3-a6f5-552b63c90b71/_apis/Identities/0adac0d0-283d-6c75-8654-828be949be3b",
+                "_links": {
+                    "avatar": {
+                        "href": "https://dev.azure.com/kmddk/_apis/GraphProfile/MemberAvatars/aad.MGFkYWMwZDAtMjgzZC03Yzc1LTg2NTQtODI4YmU5NDliZTNi"
+                    }
+                },
+                "id": "0adac0d0-283d-6c75-8654-828be949be3b",
+                "uniqueName": "KLM@kmd.dk",
+                "imageUrl": "https://dev.azure.com/kmddk/_api/_common/identityImage?id=0adac0d0-283d-6c75-8654-828be949be3b",
+                "descriptor": "aad.MGFkYWMwZDAtMjgzZC03Yzc1LTg2NTQtODI4YmU5NDliZTNi"
+            },
+            "url": "https://dev.azure.com/kmddk/2139bb34-57e3-4d7d-a6e1-1c0542a45e29/_apis/git/repositories/897458ba-1c7f-40c8-8dc0-043f083c6cd7/refs?filter=heads%2F47902_fix_f_ui_nightly_tests_pt3"
+        }],
+        "count" : 389
+    }""">
+    type private CommitRecord = JsonProvider<"""{
+        "count": 100,
+        "value": [
+            {
+                "commitId": "b1c561d96ff1808700b34b3ef5346e0df4bf5ed8",
+                "author": {
+                    "name": "Jakob Pele Leer",
+                    "email": "JL@kmd.dk",
+                    "date": "2020-04-21T07:17:10Z"
+                },
+                "committer": {
+                    "name": "Jakob Pele Leer",
+                    "email": "JL@kmd.dk",
+                    "date": "2020-04-21T07:17:10Z"
+                },
+                "comment": "Merged PR 37570: Student member submitting form about end of education only registers",
+                "commentTruncated": true,
+                "changeCounts": {
+                    "Add": 0,
+                    "Edit": 5,
+                    "Delete": 0
+                },
+                "url": "https://dev.azure.com/kmddk/2139bb34-57e3-4d7d-a6e1-1c0542a45e29/_apis/git/repositories/897458ba-1c7f-40c8-8dc0-043f083c6cd7/commits/b1c561d96ff1808700b34b3ef5346e0df4bf5ed8",
+                "remoteUrl": "https://dev.azure.com/kmddk/Gandalf/_git/MemberUI/commit/b1c561d96ff1808700b34b3ef5346e0df4bf5ed8"
+            }]
+    }""">
+    type private Repositories = JsonProvider<"""{
+      "value": [
+        {
+          "id": "897458ba-1c7f-40c8-8dc0-043f083c6cd7",
+          "name": "MemberUI",
+          "url": "https://dev.azure.com/kmddk/2139bb34-57e3-4d7d-a6e1-1c0542a45e29/_apis/git/repositories/897458ba-1c7f-40c8-8dc0-043f083c6cd7",
+          "project": {
+            "id": "2139bb34-57e3-4d7d-a6e1-1c0542a45e29",
+            "name": "Gandalf",
+            "description": "Gandalf",
+            "url": "https://dev.azure.com/kmddk/_apis/projects/2139bb34-57e3-4d7d-a6e1-1c0542a45e29",
+            "state": "wellFormed",
+            "revision": 2247,
+            "visibility": "private",
+            "lastUpdateTime": "2019-11-15T11:09:49.727Z"
+          },
+          "defaultBranch": "refs/heads/develop",
+          "size": 30778301,
+          "remoteUrl": "https://kmddk@dev.azure.com/kmddk/Gandalf/_git/MemberUI",
+          "sshUrl": "git@ssh.dev.azure.com:v3/kmddk/Gandalf/MemberUI",
+          "webUrl": "https://dev.azure.com/kmddk/Gandalf/_git/MemberUI"
+        }], 
+        "count" : 13
+    }""">
+    type private Repository ={
+        Id : string
+        Name : string
+        DefaultBranch : string
+    }
+
+    let private readBody = 
+        function
+            | Binary b -> System.Text.Encoding.ASCII.GetString b
+            | Text t -> t
+
+    let request account project path  = 
+        let url = sprintf "https://dev.azure.com/%s/%s/_apis/git/repositories%s?api-version=5.1" account project path
+        let headers =
+            [
+                HttpRequestHeaders.BasicAuth user pwd
+                HttpRequestHeaders.ContentType HttpContentTypes.Json
+            ]       
+        let resp = 
+            Http.AsyncRequest(url,
+                httpMethod = "GET", 
+                silentHttpErrors = true,
+                headers = headers
+            ) |> Async.RunSynchronously
             
-    let repoDir url = 
-        url 
-        |> hash 
-        |> sprintf "./repositories/%s" 
-        |> System.IO.Path.GetFullPath
-
-    let sync user pwd url = 
+        resp.StatusCode,resp.Body |> readBody
         
-        let repoDir =  
-            repoDir url
-
-        let ca = 
-            Handlers.CredentialsHandler(fun _ _ _ -> 
-                UsernamePasswordCredentials(Username = user, Password = pwd) :> Credentials
-            )
-        
-        if System.IO.Directory.Exists repoDir then
-            log "Deleting old repo"
-            System.IO.Directory.Delete(repoDir,true)
-
-        let options = CloneOptions()
-        options.CredentialsProvider <- ca
-        logf "Cloning repo %s into %s" url repoDir
-        Repository.Clone(url, repoDir, options) |> ignore
-        logf "Done cloning repo %s into %s" url repoDir
     
+
+    let private repositories account project = 
+        let statusCode, list = 
+            request account project ""
+        logf "Repositories: %d %s" statusCode list
+
+        if statusCode = 200 then 
+            let parsedList = 
+                list |> Repositories.Parse
+            let repos = 
+                parsedList.Value
+                |> Seq.map(fun repo -> {Id = string repo.Id; Name = repo.Name; DefaultBranch=repo.DefaultBranch})
+
+            assert(repos |> Seq.length = parsedList.Count)
+            repos
+        else
+            errorf null "Error when reading repositories. Staus: %d. Message: %s" statusCode list
+            Seq.empty
+            
     type Commit = {
         Time : System.DateTime
         Message : string
         Author : string
     }
 
-    let commits url : Commit list = 
-        try
-            let repoDir = repoDir url
-            logf "opening repo at %s" repoDir
-            use repo = new Repository(repoDir)
-            logf "Repo opened"
-            (*repo.Commits //avoid lazy evaluation since that might cause a memory overwrite issue
-            |> List.ofSeq 
-            |> List.map(fun commit ->
-                logf "reading commit: %s" commit.Id.Sha
-                {
-                    Time = commit.Author.When.ToLocalTime().DateTime
-                    Message = commit.MessageShort
-                    Author = commit.Author.Email
-                }
-            )*)
-            []
-        with e ->
-            errorf e.StackTrace "Failed when reading commits. %s" e.Message
-            []
+    let commits account project = 
+        let commits = 
+            repositories account project
+            |> Seq.collect(fun repo -> 
+                let statusCode,commits = 
+                    repo.Id |> sprintf "/%s/commits" |> request account project
+                logf "commits: %d %s" statusCode commits
+                if statusCode = 200 then 
+                    let parsedCommits = 
+                       commits |> CommitRecord.Parse
+                    let commits = 
+                        parsedCommits.Value
+                        |> Seq.map(fun commit ->
+                            {
+                                Time = commit.Author.Date.Date
+                                Message = commit.Comment
+                                Author = commit.Author.Email
+                            }
+                        )
+
+                    assert(commits |> Seq.length = parsedCommits.Count)
+                    commits
+                else
+                    errorf null "Error when reading commits of %s. Staus: %d. Message: %s" repo.Name statusCode commits
+                    Seq.empty
+            )
+        commits
            
     type BranchData = {
-        TreeName : string
         Name : string
         LifeTimeInHours : int
     }
 
-    let branches url =
-        let repoDir = repoDir url
-        use repo = new Repository(repoDir)
-        let branches = repo.Branches
-        branches
-        |> List.ofSeq
-        |> Seq.map(fun branch -> 
-            let commits = 
-                branch.Commits
-                |> Seq.sortBy(fun c -> c.Author.When)
-            let branchLifeTimeInHours = 
-                ((commits |> Seq.maxBy(fun c -> c.Author.When)).Author.When
-                 - (commits |> Seq.head).Author.When).TotalMinutes / 60.
-                 |> int
-            let treeName,branchName = 
-                match branch.FriendlyName.Split("/") with
-                [|a|] -> "",a
-                | [|treeName;branchName|] -> treeName,branchName
-                | tags -> 
-                    let tags = tags |> Array.tail
-                    tags |> Array.head, System.String.Join("/", tags |> Array.tail)
-           
-            {
-                Name = branchName
-                TreeName = treeName
-                LifeTimeInHours = branchLifeTimeInHours
-            }
-         ) 
-        
-        
+    let branches account project =
+       repositories account project
+       |> Seq.collect(fun repo -> 
+            let statusCode,branches = 
+                repo.Id |> sprintf "/%s/refbrancs" |> request account project
+            logf "branches: %d %s" statusCode branches
+            if statusCode = 200 then 
+                let parsedBranches = 
+                   branches |> BranchList.Parse
+                let branches = 
+                    parsedBranches.Value
+                    |> Seq.filter(fun branch -> branch.Name.StartsWith "ref/heads/")
+                    |> Seq.map(fun branch ->
+                        let statusCode,commits = 
+                            repo.Id |> sprintf "/%s/commitsbranches" |> request account project
+                        logf "commit batch: %d %s" statusCode commits
+                        let branchLifeTimeInHours = 
+                            if statusCode = 200 then
+                                let parsedCommits = 
+                                   commits |> CommitBatch.Parse
+                                let commits = 
+                                    parsedCommits.Value
+                                    |> Seq.map(fun commit ->
+                                        {
+                                            Time = commit.Author.Date.Date
+                                            Message = commit.Comment
+                                            Author = commit.Author.Email
+                                        }
+                                    ) |> Seq.sortBy (fun c -> c.Time)
+
+                                assert(commits |> Seq.length = parsedCommits.Count)
+                                ((commits |> Seq.maxBy(fun c -> c.Time)).Time
+                                 - (commits |> Seq.head).Time).TotalMinutes / 60.
+                                 |> int
+                                 |> Some
+                            else
+                                errorf null "Error when reading commit batch of %s. Staus: %d. Message: %s" branch.Name statusCode commits
+                                None
+                        
+                        branch.Name.Substring("ref/heads/".Length),branchLifeTimeInHours
+                    ) |> Seq.filter(snd >> Option.isSome)
+                    |> Seq.map(fun (name,lifetime) ->
+                        {
+                            Name = name
+                            LifeTimeInHours = lifetime.Value
+                        }
+                    )
+
+                branches
+            else
+                errorf null "Error when reading commits of %s. Staus: %d. Message: %s" repo.Name statusCode branches
+                Seq.empty
+        )
--- collectors/collectors.Git/src/Services/Data.fs
+++ collectors/collectors.Git/src/Services/Data.fs
@@ -6,11 +6,13 @@ open Hobbes.Helpers.Environment
 [<RouteArea ("/data", false)>]
 module Data =
     let inline private toCsList (lines : seq<string>) = 
-        System.String.Join(",",lines)
+        System.String.Join(",",lines |> Seq.map (sprintf """ "%s" """))
 
     type Config = FSharp.Data.JsonProvider<"""{
         "source" : "git",
         "dataset" : "commits",
+        "account" : "kmddk",
+        "project" : "flowerpot",
         "urls" : ["hkjh","ljlkhj"],
         "credentials" : {
             "user" : "lll",
@@ -23,45 +25,38 @@ module Data =
        let conf = Config.Parse confDoc
        match env conf.Credentials.User null, env conf.Credentials.Pwd null with
        null, _ | _, null -> 403, "Not authorized for repository"
-       | user, pwd ->
-           conf.Urls |> Seq.map (fun url ->
-               async {
-                   Collector.Git.Reader.sync user pwd url
-               }
-           ) |> Async.Parallel
-           |> Async.RunSynchronously 
-           |> ignore
+       | _ ->
            200,"Synced"
 
     [<Post ("/read", true)>]
     let read confDoc =
         let conf = Config.Parse confDoc
+        let dataset = conf.Dataset.ToLower()
         let columnNames,rows =
-            let url = conf.Urls.[0]
-            match conf.Dataset.ToLower() with
+            match dataset with
             "commits" -> 
                 [
                     "timestamp"
                     "message"
                     "author"
-                ],  (*conf.Urls
-                    |> Seq.collect*)
-                    (//fun url ->
-                        let commits = Collector.Git.Reader.commits url
-                        commits |> Seq.map(fun commit ->
-                            Hobbes.Web.Log.logf "reading commit: %s" commit.Message
-                            commit.Time.ToString()::commit.Message::[commit.Author]
-                        )
+                ],  
+                    let commits = Collector.Git.Reader.commits conf.Account conf.Project
+                    Hobbes.Web.Log.logf "formatting commits %A" commits
+                    commits |> Seq.map(fun commit ->
+                        Hobbes.Web.Log.logf "reading commit: %s" commit.Message
+                        commit.Time.ToString()::commit.Message::[commit.Author]
                     )
             | "branches" -> 
+
+                let branches = 
+                    Collector.Git.Reader.branches conf.Account conf.Project
+                Hobbes.Web.Log.logf "formatting branches %A" branches
                 [
-                    "TreeName"
                     "BranchName"
                     "LifeTimeInHours"
-                ], conf.Urls
-                   |> Seq.collect Collector.Git.Reader.branches
+                ], branches
                    |> Seq.map(fun branch ->
-                       branch.TreeName::branch.Name::[branch.LifeTimeInHours.ToString()]
+                       branch.Name::[branch.LifeTimeInHours.ToString()]
                    )
             | _ -> failwith "unknown dataset"
         let values = 
@@ -69,7 +64,9 @@ module Data =
                     |> Seq.map(toCsList >> sprintf "[%s]"))
         let res = 
             sprintf """{
-                "columnNames": [%s] 
-                "values" : [%s]
-            } """ (toCsList columnNames) values
+               "searchKey" : "%s",
+               "columnNames" : %s,
+               "rows" : %s,
+               "rowCount" : %d
+               }""" ("git" + conf.Project + dataset) (toCsList columnNames) values (rows |> Seq.length)
         200,res

54ecc542f2e8610aa936639b8743bf1df4b1c8c5
Rune Lund-Søltoft <funchsoltoft@gmail.com>
2020-05-12 15:28:28 +0200

    server should do any transformation

--- services/hobbes.server/src/Services/Data.fs
+++ services/hobbes.server/src/Services/Data.fs
@@ -13,27 +13,7 @@ module Data =
     [<Get ("/csv/%s")>]
     let csv configuration =  
         debugf "Getting csv for '%A'" configuration
-        match Http.get (configuration |> Http.Calculate |> Http.Calculator) (Cache.CacheRecord.Parse >> Cache.readData)  with
-        Http.Success rows ->
-            let csv = 
-                let columnNames = 
-                    (":",rows
-                         |> Seq.collect(snd >> (Seq.map fst))
-                         |> Seq.distinct
-                         |> Seq.map(fun v -> v.Replace(":",";"))
-                    ) |> System.String.Join
-                let values = 
-                    ("\n",rows
-                          |> Seq.map(fun (_,row) ->
-                              (":",row
-                                   |> Seq.map(fun (_,v) ->
-                                       let s = string v
-                                       s.Replace(":",";")
-                                   )
-                              ) |> System.String.Join
-                          )
-                    ) |> System.String.Join 
-                columnNames + "\n" + values
-            debugf "CSV: %s" csv
+        match Http.get (configuration |> Http.Calculate |> Http.Calculator) id with
+        Http.Success csv ->
             200, csv
         | Http.Error(sc,m) -> sc,sprintf "Data for configuration %s not found. Message: %s" configuration m

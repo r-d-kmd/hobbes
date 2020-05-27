0e6c05cecf2d94e57a4adfaa7a13a773fc5e9783
Rune Lund-Søltoft <funchsoltoft@gmail.com>
2020-05-27 13:21:25 +0200

    Started setting up kustomize, so that we can do auto deploy on code _and_ infrastructure

--- tools/workbench/src/production transformations/Git.fs
+++ tools/workbench/src/production transformations/Git.fs
@@ -1,6 +1,6 @@
 namespace Workbench.Transformations
 
-[<Workbench.Transformations(Workbench.Project.Flowerpot ||| Workbench.Project.Branches)>]
+[<Workbench.Transformations(Workbench.Project.Flowerpot)>]
 module Git =
 
     open Hobbes.Parsing.AST

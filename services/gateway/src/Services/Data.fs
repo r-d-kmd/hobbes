a0050bcfa0ce4fc7a825bb9fe8da7a4044c29c2c
Lucas Petersen <lcp@kmd.dk>
2020-06-12 13:46:27 +0200

    Pinned saturn to v 0.13.1, formatted json data

--- services/gateway/src/Services/Data.fs
+++ services/gateway/src/Services/Data.fs
@@ -17,7 +17,13 @@ module Data =
         Http.Success config -> 
             match Http.get (config.Source |> RawdataTypes.keyFromSource |> Http.CacheService.Read |> Http.UniformData) id with
             Http.Success json ->
-                let data = Json.deserialize<Cache.DataResult> json
-                200, """[{"id" : "ljkjkkkj",-1,"2019-12-31"},{"id" : "ljklkj",4,"2019-12-31"}]"""
+                let record = Json.deserialize<Cache.CacheRecord> json
+                let names = record.Data.ColumnNames
+                let rows = record.Data.StringRows()
+                let formatted = Array.map (fun vals -> Array.map2 (fun n v -> sprintf "\"%s\" : %s" n v ) names vals
+                                                       |> String.concat ",") rows
+                                |> String.concat "},{"
+                                |> sprintf "[{%s}]"
+                200, formatted
             | Http.Error(sc,m) -> sc,sprintf "Data for configuration %s not found. Message: %s" configuration m
         | Http.Error(sc,m) -> sc,sprintf "Configuration %s not found. Message: %s" configuration m

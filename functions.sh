77e4a4522053c4c5e9500e7e23d0eae6d7fcff18
Mikkel Ditlevsen (DIT) <DIT@kmd.dk>
2020-07-03 14:00:32 +0200

    functions applyProductionYaml correctly

--- functions.sh
+++ functions.sh
@@ -312,7 +312,7 @@ function applyProductionYaml() {
     cd $KUBERNETES_DIR
     mv kustomization.yaml ./local_patches/kustomization.yaml
     mv ./prod_patches/kustomization.yaml kustomization.yaml
-    kustomize build -o test.yaml
+    kubectl apply -k . -o yaml > test.yaml
     mv kustomization.yaml ./prod_patches/kustomization.yaml
     mv ./local_patches/kustomization.yaml kustomization.yaml
     cd $CURRENT_DIR

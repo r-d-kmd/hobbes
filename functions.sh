ffeee8843b8f07ca8891897f42b3b575a6338161
Rune Lund-Søltoft <funchsoltoft@gmail.com>
2020-10-13 13:23:53 +0200

    removing alias as a test for the build server

--- functions.sh
+++ functions.sh
@@ -331,15 +331,7 @@ function forward() {
     kc port-forward "service/$1" $2:$2 &
 }
 
-alias kc=kubectl
-alias logs="kc logs"
-alias get="kc get"
-alias pods="get pods"
-alias jobs="get jobs"
-alias services="get services"
-alias delete="kc delete"
-alias apply="kc apply"
-alias describe="kc describe"
+
 
 
 printf "Project home folder is:\n"

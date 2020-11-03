be06db4e49ffa28a7086f0a61f259bb0ff8c937e
mikkel ditlevsen <midi@itu.dk>
2020-11-03 18:54:34 +0100

    reverted change, didnt work

--- build.fsx
+++ build.fsx
@@ -60,7 +60,7 @@ let runOrDefaultWithArguments =
     targetName
     >> Target.runOrDefaultWithArguments 
 
-let dockerOrg = "hobbes"
+let dockerOrg = "hobbes.azurecr.io"
 let run command workingDir args = 
     let arguments = 
         match args |> String.split ' ' with

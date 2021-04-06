8041741d57b0d14c1a68c769ab0a5d735026d45b
Rune Lund-Søltoft <rsl@kmd.dk>
2021-04-06 18:07:27 +0200

    Debugging build

--- tests/build.fsx
+++ tests/build.fsx
@@ -114,6 +114,7 @@ let forwardServicePort serviceName here there =
     try
         kubectl true "wait" (sprintf "--for=condition=ready pod -l app=%s --timeout=120s" serviceName) |> ignore
     with _ -> 
+        kubectl false "describe" <| sprintf "pod -l %s" serviceName |> ignore
         kubectl false "logs" <| sprintf "service/%-svc" serviceName |> ignore
     try
         kubectlf "port-forward" (sprintf "service/%s %d:%d" serviceName here there)
